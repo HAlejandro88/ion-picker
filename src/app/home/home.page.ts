d945374a35a4d581ea4164fd33030c7be5d5047c
Alejandro <alexsotog82@gmail.com>
2020-01-07 18:24:53 -0600

    ion-picker

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { async } from '@angular/core/testing';
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  framework = '';
+
+  constructor(private pickerCtrl: PickerController) {}
+
+  async showBasic() {
+    let opts: PickerOptions = {
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Done'
+        }
+      ],
+      columns: [
+        {
+          name: 'framework',
+          options: [
+            {text: 'Angular', value: 'A'},
+            {text: 'Vue', value: 'V'},
+            {text: 'React', value: 'R'}
+          ]
+        }
+      ]
+    };
+    let picker = await this.pickerCtrl.create(opts);
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let col = await picker.getColumn('framework');
+      console.log('col',col);
+      this.framework = col.options[col.selectedIndex].text;
+    })
+  }
 
 }
